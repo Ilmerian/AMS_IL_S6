b76f4066fcd38b26d5360a6a059cdb2219bfafd8
Deniz Ertekin <kingden01@yandex.ru>
2025-12-11 03:19:37 +0100

    Build successful, ready for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,26 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    dedupe: ['react', 'react-dom', '@emotion/react', '@emotion/styled']
-  }  
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'mui-vendor': ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
+          'i18n-vendor': ['react-i18next', 'i18next'],
+          'supabase-vendor': ['@supabase/supabase-js'],
+          'utils-vendor': ['@dnd-kit/core', '@dnd-kit/sortable', '@dnd-kit/utilities']
+        }
+      }
+    }
+  },
+  server: {
+    port: 5173
+  }
 })
